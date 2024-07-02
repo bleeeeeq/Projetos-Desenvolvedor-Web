7ca604df3e9983a648589666674268037eda470b
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-02 18:16:49 -0300

    Exercício 2 JavaScript
    Exercício que analisa a idade digitada pelo usuário, verificando se a idade é maior que 18 anos

---
+++ JavaScript/Ex-2.js
@@ -0,0 +1,11 @@
+// Exercício [Idade Permitida] de JS
+
+let num = parseFloat(window.prompt('Digite sua Idade:'))
+
+if(num > 18 ){
+    alert('Entrada permitida!')
+}
+
+else{
+    alert('Entrada negada!')
+}
