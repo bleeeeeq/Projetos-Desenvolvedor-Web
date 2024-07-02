d7ab780442485f687c00a4fd9b92ff8dc343f6b5
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-02 17:38:02 -0300

    Exercício 2 JavaScript
    Atualização do exercício 2, utilizando o JavaScript externo para criar uma calculadora que será apresentada em forma de "alertas" ao usuário

--- JavaScript/EXE2.2.js
+++ JavaScript/EXE2.2.js
@@ -1,1 +1,29 @@
-alert("Teste")
+// Exercício de JS
+
+let num1 = parseFloat(window.prompt('Digite um número:'))
+let num2 = parseFloat(window.prompt('Digite outro número:'))
+let operacao = window.prompt('Qual operação deseja realizar: \n Para ADIÇÃO digite: '+' + \n Para SUBTRAÇÃO digite: - \n Para MULTIPLICAÇÃO digite: *\n Para DIVISÃO digite: /'+' \n Para MÓDULO digite: %')
+
+if(operacao =='+'){
+    let result = num1+num2
+    alert(result)
+}
+else if(operacao =='-'){
+    let result = num1-num2
+    alert(result)
+}
+else if(operacao =='*'){
+    let result = num1*num2
+    alert(result)
+}
+else if(operacao =='/'){
+    let result = num1/num2
+    alert(result)
+}
+else if(operacao =='%'){
+    let result = num1%num2
+    alert(result)
+}
+else{
+    alert('Opção invalida')
+}
