357794849c52808f47c226f414a9b0e2c7bcae8a
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-02 18:18:16 -0300

    Exercício 3 JavaScript
    Exercício que analisa a idade, porém diferentemente do anterior, ele tem uma verificação utilizando o "&&" que irá depender se a idade digitada estará entre duas idades distintas, nesse caso, entre 6 e 60 anos
    
    Além de um breve resumo do porquê usar "&&" ao invés de "&"

---
+++ JavaScript/Ex-3.js
@@ -0,0 +1,16 @@
+// Exercício [Idade Gratuita] de JS
+
+let idade = parseFloat(window.prompt("Digite sua Idade:"))
+
+if(idade <= 6){
+    alert("Não paga passagem")
+    alert("Seja Bem Vindo!!")
+}
+
+else if(idade > 6 && idade <= 60){
+    alert("Paga passagem")
+}
+
+// O uso do "&&" ao invés de apenas "&" se trata do uso "padrão" do "&&" em operações lógicas.
+// O "&" geralmente atua em operações bit a bit, avaliando cada bit individualmente.
+// Para garantir a avaliação correta de condições booleanas, use sempre "&&".
