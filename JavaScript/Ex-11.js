dbbc18f903493a7e94a124d2ede9c8dd7a7c2d27
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-04 17:43:13 -0300

    Criação do exercício 11
    Exercício utilizando o "case" para exibir o dia da semana

---
+++ JavaScript/Ex-11.js
@@ -0,0 +1,41 @@
+let num = window.prompt("Digite um número de 1 a 7:")
+
+if (/^[0-9.,]+$/.test(num)) {
+
+    switch(num) {
+        case '1':
+            alert('Domingo, o primeiro da semana!')
+            break
+
+        case '2':
+            alert('Segunda-Feira, o segundo da semana!')
+            break
+
+        case '3':
+            alert('Terça-Feira, o terceiro da semana!')
+            break
+
+        case '4':
+            alert('Quarta-Feira, quarto da semana!')
+            break
+
+        case '5':
+            alert('Quinta-feira, o quinto da semana!')
+            break
+
+        case '6':
+            alert('Sexta-Feira, o sexto da semana!')
+            break
+            
+        case '7':
+            alert('Sábado, o sétimo da semana!')
+            break
+
+        default:
+            alert("O número digitado não está entre 1 e 7! \nTente novamente.")
+    }
+}
+
+else {
+    alert("É necessário que você digite um algarismo! \n~> Exemplo: '4' <~")
+}
