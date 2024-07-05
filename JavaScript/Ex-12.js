e180a16dc8f5b603670b3d243e439e4690773e83
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-05 11:24:18 -0300

    Criação do exercício 12
    Exercício que mostra ao usuário os números pares entre 0 e 10, isso com o uso do loop (for)

---
+++ JavaScript/Ex-12.js
@@ -0,0 +1,15 @@
+// Exercício [Loop pares 0-10] de JS
+
+for (let i = 0; i <= 10; i+=2){
+    console.log (i)
+}
+
+/* Outro método de resolução
+
+for (let i = 0; i <= 10; i++){
+    par = i % 2
+    if (par == 0){
+    console.log (i)
+    }
+}
+*/
