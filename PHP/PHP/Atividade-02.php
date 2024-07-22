e1ef76646b422fca77ed45617463be0f92b7862d
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-22 10:52:37 -0300

    Finalização exercício 2
    Correção que agora faz com que o exercício funcione

--- PHP/PHP/Atividade-02.php
+++ PHP/PHP/Atividade-02.php
@@ -1,16 +1,16 @@
 <?php
 
-if(isset(&_GET['sorteio'])){
-    &num = rand(0,100);
+if(isset($_GET['sorteio'])){
+    $num = rand(0,100);
     //echo"$num"
     echo "<script>
-        if(confirm("O número sorteado é: $num, deseja um novo número?"){
+        if(confirm('O número sorteado é: $num, deseja um novo número?')){
             location.reload()
         }
         else{
-            window.location.href = '../index.html'
+            window.location.href = '../HTML/index.html'
         }
     </script>";
-}
+    }
 
 ?>
