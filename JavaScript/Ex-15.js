780ac35ef7a9c95f1b202d18910e0ced6b7edc11
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-22 21:23:00 -0300

    Adição exercício 15

--- JavaScript/Ex-15.js
+++ JavaScript/Ex-15.js
@@ -1,21 +1,19 @@
-// Exercício [Contagem regressiva] de JS
+// Exercício 15 [Contagem regressiva] de JS
 
-let contador = 10
-let resultado = ""
+let contador = 10;
+let resultado = "";
 
-while (contador >=1){
-    resultado += contador + "\n"
-    contador--
+while (contador >= 1) {
+    resultado += contador + "\n";
+    contador--;
 }
 
-resultado += "Contagem finalizada"
+resultado += "Contagem finalizada";
 
-// Esperar o DOM estar carregado antes de manipular a textarea
-document.addEventListener('DOMContentLoaded', function() {
-    let textarea = document.querySelector('textarea');
-    if (textarea) {
-        textarea.textContent = contador;
-    } else {
-        console.error('Textarea não encontrada no documento.');
-    }
-});
+let textarea = document.getElementById('resultado');
+if (textarea) {
+    textarea.textContent = resultado;
+    textarea.style.display = 'block';
+} else {
+    console.error('Textarea não encontrada no documento.');
+}
