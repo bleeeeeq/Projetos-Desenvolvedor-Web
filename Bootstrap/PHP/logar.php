84e5d9fcd7f4487ff9f01dc75c91475671c3589f
Gabriel <gabrielmarotacampos221@gmail.com>
2024-07-26 11:56:00 -0300

    Atualização "logar.php"
    Agora é possível logar no sistema, e após o login bem sucedido, o usuário será enviado para a tela "bemVindo.php"

--- Bootstrap/PHP/logar.php
+++ Bootstrap/PHP/logar.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 include 'database.php';
-
 $error_message = '';
+
 if (isset($_POST['logar'])) {
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
     $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
@@ -15,7 +16,29 @@ if (isset($_POST['logar'])) {
     } 
 
     else {
-        // Código de login, quando for feito
+        $stmt = $conexao -> prepare ('select nome, idusuario from usuario where email = ? and senha = ?');
+        $stmt -> bind_param("ss", $email, $senha);
+        $stmt -> execute();
+        $result = $stmt -> get_result();
+
+        if ($result -> num_rows == 1){
+            $usuario = $result -> fetch_assoc();
+            $_SESSION ['id'] = $usuario ['idusuario'];
+            $_SESSION ['nome'] = $usuario ['nome'];
+            echo "<script>
+                alert ('Usuário verificado com sucesso! Bem vindo ao sistema, ".$usuario['nome']."!')
+                window.location.href='../HTML/bemVindo.php'
+            </script>";
+            exit;
+        }
+
+        else{
+            echo "<script>
+            alert ('Email ou senha incorretos. Tente novamente.')
+            window.location.href='../HTML/login.html'
+        </script>";
+            exit;
+        }
     }
 }
 
