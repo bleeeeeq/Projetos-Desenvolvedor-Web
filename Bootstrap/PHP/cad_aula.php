5d0d5f2e4faf9a3a95e29cf7044bbec3a70b5197
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-07 11:51:59 -0300

    Criação do cadastro aula (PHP)
    O PHP que permite que aulas sejam cadastradas do zero

---
+++ Bootstrap/PHP/cad_aula.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include 'database.php';
+
+    if(isset($_POST['agendar'])){
+        $instrutor = filter_input(INPUT_POST, 'instrutor', FILTER_SANITIZE_STRING);
+        $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
+        $aluno = filter_input(INPUT_POST, 'aluno', FILTER_SANITIZE_STRING);
+        $data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
+        $hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
+        $pago = filter_input(INPUT_POST, 'pago', FILTER_SANITIZE_STRING);
+        $veiculo = filter_input(INPUT_POST, 'veiculo', FILTER_SANITIZE_STRING);
+
+        if(empty($instrutor) || empty($cpf) || empty($aluno) || empty($data) || empty($hora) || empty($pago) || empty($veiculo)){
+            echo"<script>
+                    alert('Algum campo ainda está em branco!');
+                </script>";
+            exit;
+        }
+        else{
+            $declaracaoBD = $conexao -> prepare("INSERT INTO `aula`(`data`,`hora`,`instrutor`,`aluno`,`veiculo`,`pago`,`cpf`)VALUES(?,?,?,?,?,?,?)");
+            $declaracaoBD -> bind_param("sssssss", $data, $hora, $instrutor, $aluno, $veiculo, $pago, $cpf);
+            if ($declaracaoBD -> execute()){
+                echo "<script>
+                        alert('Aula cadastrada com sucesso!');
+                        window.location.href = '../HTML/cadastrarAula.php';
+                    </script>;";
+            }
+            else{
+                echo "Erro ao cadastrar a aula: ". $declaracaoBD->error;
+            }
+        }
+    }
+?>
