2dd2433c1d5aaab19b614b0f46b64b14434c6822
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-12 11:42:13 -0300

    Criação do menu
    Apenas o início, ainda está para ser utilizado em outras abas

---
+++ Bootstrap/HTML/navegador.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="menu-container">
+        <a href="bemVindo.php">Início</a>
+        <a href="cad_aluno.php">Cadastrar Aluno</a>
+        <a href="#">Cadastrar Carro</a>
+        <a href="cadastrarAula.php">Cadastrar Aula</a>
+        <a href="tabelaAula.php">Buscar Aula</a>
+        <a href="#">Contas</a>
+        <a href="#">Sair</a>
+    </nav>
+</body>
+</html>
