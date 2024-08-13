877ba11f2d0192abb07465d23b50bac20c54099b
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-13 11:03:40 -0300

    Backup banco de dados (13.08)

---
+++ Bootstrap/Aleatório/Database (13.08).sql
@@ -0,0 +1,128 @@
+CREATE DATABASE  IF NOT EXISTS `autoescola` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `autoescola`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: autoescola
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aula`
+--
+
+DROP TABLE IF EXISTS `aula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aula` (
+  `idaula` int NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `hora` time NOT NULL,
+  `instrutor` varchar(45) NOT NULL,
+  `aluno` varchar(60) NOT NULL,
+  `veiculo` varchar(45) NOT NULL,
+  `pago` varchar(45) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
+  PRIMARY KEY (`idaula`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aula`
+--
+
+LOCK TABLES `aula` WRITE;
+/*!40000 ALTER TABLE `aula` DISABLE KEYS */;
+INSERT INTO `aula` VALUES (1,'2024-07-31','17:45:00','julinhadograu','igu_IM','peugeot 207','sim','123.456.789-11'),(3,'2025-07-31','20:30:00','Abílio Abelhudo','Tarças C#','Kombi','sim','321.132.312-41'),(5,'2025-07-31','20:30:00','Abílio Abelhudo','Kleiton Conká','Abelhomóvel','nao','321.132.312-45'),(6,'2024-08-27','17:10:00','abio abeia','abeia abeia','pitbull','nao','321.421.999-84'),(22,'2025-07-31','20:30:00','Abílio Abelhudo','Abílio Abelhudo','Kombi','SIM!','213.123.123-45'),(23,'2025-07-31','20:30:00','Abílio Abelhudo','Abílio Abelhudo','Kombi','SIM!','213.123.123-45'),(24,'2025-07-31','20:30:00','Abílio Abelhudo','Abílio Abelhudo','Kombi','SIM!','213.123.123-45'),(25,'2025-07-31','20:30:00','Abílio Abelhudo','Abílio Abelhudo','Kombi','SIM!','213.123.123-45'),(26,'2025-07-31','20:30:00','Abílio Abelhudo','Abílio Abelhudo','Kombi','SIM!','213.123.123-45');
+/*!40000 ALTER TABLE `aula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cadaluno`
+--
+
+DROP TABLE IF EXISTS `cadaluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cadaluno` (
+  `idcadaluno` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) DEFAULT NULL,
+  `cpf` varchar(15) DEFAULT NULL,
+  `celular` varchar(16) DEFAULT NULL,
+  `whatsapp` varchar(45) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcadaluno`),
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cadaluno`
+--
+
+LOCK TABLES `cadaluno` WRITE;
+/*!40000 ALTER TABLE `cadaluno` DISABLE KEYS */;
+INSERT INTO `cadaluno` VALUES (1,'igu_IM','123.456.789-11','329123458','não','iguunderlineIM@gmail.com','D'),(2,'Juninho Crey Crey','999.888.777-66','32993939393','sim','juninbalademais@gmail.com','E');
+/*!40000 ALTER TABLE `cadaluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `sobrenome` varchar(70) DEFAULT NULL,
+  `sexo` varchar(45) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  `endereco` varchar(80) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'dwadwadwad','dwadwa','MA','dawdaw@dwa.dwa','dawdaw','dwadaw','dawdaw','AC'),(2,'igu','underlineIM','MA','iguzim@gmail.com','iguiguigu','220 ou 226, vc q sabe','jf city','MG');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'autoescola'
+--
+
+--
+-- Dumping routines for database 'autoescola'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-13 11:02:59
