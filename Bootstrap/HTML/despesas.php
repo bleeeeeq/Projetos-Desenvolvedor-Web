a4b46cebec022e629f60ecf75d5c6a5394e16c53
Gabriel <gabrielmarotacampos221@gmail.com>
2024-08-15 11:50:46 -0300

    Atualização em despesas.php
    Atualização que faz a data ser limitada, tornando assim impossível de selecionar datas após o dia atual, e antes de 30 dias atrás

--- Bootstrap/HTML/despesas.php
+++ Bootstrap/HTML/despesas.php
@@ -1,6 +1,9 @@
+<?php
+    session_start();
+?>
 <!-- CRIANDO AS LISTAS -->
 <?php
-    $despesas = ["Depesas com Funcionário"=>["Salario","Vale transporte","Vale alimentação"],"Despesas com Carros"=>["Oficina","Gasolina","Lavagem"],"Despesas Fixas"=>["Luz","Agua","Aluguel"],"Despesas Eventuais"=>["Solicitar autorizaçao"]];
+    $despesas = ["Depesas com Funcionário"=>["Salário","Vale transporte","Vale alimentação"],"Despesas com Carros"=>["Oficina","Gasolina","Lavagem"],"Despesas Fixas"=>["Luz","Água","Aluguel"],"Despesas Eventuais"=>["Solicitar autorização"]];
 
     $tipoSelecionado = $_POST["tipo"]??'';
     $descricoes = $tipoSelecionado?$despesas[$tipoSelecionado]:[];
@@ -31,7 +34,9 @@
 
             <?php endforeach ?>
         </select>
+    </form>
 
+    <form action="../PHP/contas.php" method="POST">
         <label for="descricao">Descrição:</label>
         <select name="descricao" id="descricao" required>
             <?php
@@ -49,10 +54,16 @@
         <label for="valor">Valor:</label>
         <input type="number" id="valor" name="valor" step="0.01" required>
 
+        <?php 
+        $min = new DateTime(); 
+        $min -> modify ('-30 days');
+        $max = new DateTime();
+        $max -> modify ('0 days');
+        ?>
         <label for="data">Data:</label>
-        <input type="date" id="data" name="data" required>
+        <input type="date" id="data" name="data" <?php date('Y-m-d');?> min =<?=$min->format('Y-m-d')?> max =<?=$max->format('Y-m-d')?> required>
 
-        <button class="button" type="submit">Cadastrar</button>
+        <button class="button" type="submit" name="btnCad">Cadastrar</button>
     </form>
 
     <h2>Lista de Despesas</h2>
