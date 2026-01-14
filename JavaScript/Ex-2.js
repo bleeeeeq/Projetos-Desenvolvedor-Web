9a24b402d094f09d2cace68fe5be1a820eaaaa20
Gabriel <gabrielmarotacampos221@gmail.com>
2026-01-14 17:12:23 -0300

    Adição setTimeout
    Segura o loading da página permitindo que o conteúdo carregue antes de cancelar tudo

--- JavaScript/Ex-2.js
+++ JavaScript/Ex-2.js
@@ -1,11 +1,17 @@
 // Exercício [Idade Permitida] de JS
 
-let num = parseFloat(window.prompt('Digite sua Idade:'))
+setTimeout(() => {
+    let num = parseFloat(window.prompt('Digite sua Idade:'))
 
-if(num > 18 ){
-    alert('Entrada permitida!')
-}
+    if(num >= 18 & num <=149){
+        alert('Entrada permitida!')
+    }
 
-else{
-    alert('Entrada negada!')
-}
+    else if(num>150){
+        alert('Mais um exemplo de um Cephalon do Warframe ou um ser ancestral.')
+    }
+
+    else{
+        alert('Entrada negada!')
+    }
+},100)
