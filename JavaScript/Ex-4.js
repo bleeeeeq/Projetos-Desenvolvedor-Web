4b5a4ba03a95aee210d49c14e86ad7a208f42ef9
Gabriel <gabrielmarotacampos221@gmail.com>
2026-01-14 17:13:42 -0300

    Correção na lógica + adição de um extra na idade

--- JavaScript/Ex-4.js
+++ JavaScript/Ex-4.js
@@ -6,8 +6,12 @@ if (idade < 16){
     alert("Você não pode votar!")
 }
 
-if (idade >= 18 && idade < 80){
+else if (idade >= 18 && idade < 80){
     alert("Você deve votar!")
+}
+
+else if (idade >150){
+    alert("Ou você um Cephalon do Warframe ou um ser ancestral.")
 }
 
 else {
